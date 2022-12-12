5c1ba8abf8dd580f9966c5b3b110acaae1027672
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-12-12 09:22:05 -0300

    Feat: New exercise added and solved

---
+++ typescript/3_leap/leap.ts
@@ -0,0 +1,7 @@
+export function isLeap(year: number) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+      return false;
+    } else return true;
+  } else return false;
+}
